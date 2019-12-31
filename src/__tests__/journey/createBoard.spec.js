77fe934bf4783f91f1603bc9aa0d72cb58272cb6
Alany Maria <lamariasilva@MacBook-Pro-de-Alany.local>
2019-12-31 11:01:30 -0300

    Adds journey test to create a new board

--- src/__tests__/journey/createBoard.spec.js
+++ src/__tests__/journey/createBoard.spec.js
@@ -1,5 +1,7 @@
 describe('Create a board', function () {
-  it('Does not do much!', function () {
-    expect(true).to.equal(true);
+  it('Visits home page and when click on `New board` redirect to board page', function () {
+    cy.visit('http://localhost:3000');
+    cy.contains('New board').click();
+    cy.url().should('include', '/board');
   });
 });
